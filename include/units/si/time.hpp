3fe2d62cca4bcbbce355a63f98b152bee08d6685
Tim Wiesner <tim.wiesner@st.ovgu.de>
2020-02-02 16:40:34 +0100

    Add time quantity

---
+++ include/units/si/time.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "units/si.hpp"
+
+namespace
+{
+constexpr auto MicrosecondsPerSecond = 1'000'000;
+constexpr auto MillisecondsPerSecond = 1'000;
+constexpr auto SecondsPerMinute = 60;
+constexpr auto SecondsPerHour = 3600;
+} // namespace
+
+namespace units::si
+{
+using Time = Value<SiUnit<0, 0, 1, 0, 0, 0, 0>>;
+} // namespace units::si
+
+constexpr units::si::Time operator"" _us(const long double magnitude)
+{
+    return units::si::Time(magnitude / MicrosecondsPerSecond);
+}
+
+constexpr units::si::Time operator"" _ms(const long double magnitude)
+{
+    return units::si::Time(magnitude / MillisecondsPerSecond);
+}
+
+constexpr units::si::Time operator"" _s(const long double magnitude)
+{
+    return units::si::Time(magnitude);
+}
+
+constexpr units::si::Time operator"" _m(const long double magnitude)
+{
+    return units::si::Time(magnitude * SecondsPerMinute);
+}
+
+constexpr units::si::Time operator"" _h(const long double magnitude)
+{
+    return units::si::Time(magnitude * SecondsPerHour);
+}
