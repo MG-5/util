6a1b31c6564b842bf402e0cd53b3c0df652db6f8
Tim Wiesner <tim.wiesner@st.ovgu.de>
2020-02-12 15:48:58 +0100

    Add user-defined literals for Scalar class

--- include/units/si/scalar.hpp
+++ include/units/si/scalar.hpp
@@ -6,3 +6,13 @@ namespace units::si
 {
 using Scalar = Value<SiUnit<0, 0, 0, 0, 0, 0, 0>>;
 } // namespace units::si
+
+constexpr units::si::Scalar operator""_(const long double magnitude)
+{
+    return units::si::Scalar(magnitude);
+}
+
+constexpr units::si::Scalar operator""_(const unsigned long long int magnitude)
+{
+    return units::si::Scalar(magnitude);
+}
