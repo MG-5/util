fce211d05e3e220bdc40efbfd00554e38f059567
Maximilian Grau <maximilian.grau@st.ovgu.de>
2021-05-15 20:15:16 +0200

    Add velocity scales

--- include/units/si/velocity.hpp
+++ include/units/si/velocity.hpp
@@ -5,12 +5,19 @@
 namespace
 {
 constexpr auto KilometresPerHourFactor = 3.6f;
-}
+constexpr auto MilesPerHourFactor = 2.237f;
+} // namespace
 
 namespace units::si
 {
 using Velocity = Value<SiUnit<1, 0, -1, 0, 0, 0, 0>>;
 } // namespace units::si
+
+namespace units::si::scale
+{
+constexpr auto km_h = Scale<Velocity::Unit>{KilometresPerHourFactor};
+constexpr auto mi_h = Scale<Velocity::Unit>{MilesPerHourFactor};
+} // namespace units::si::scale
 
 constexpr units::si::Velocity operator"" _m_s(const long double magnitude)
 {
@@ -20,4 +27,9 @@ constexpr units::si::Velocity operator"" _m_s(const long double magnitude)
 constexpr units::si::Velocity operator"" _km_h(const long double magnitude)
 {
     return units::si::Velocity(magnitude / KilometresPerHourFactor);
+}
+
+constexpr units::si::Velocity operator"" _mi_h(const long double magnitude)
+{
+    return units::si::Velocity(magnitude / MilesPerHourFactor);
 }
