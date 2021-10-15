a59109e9b44f0e4edf616401e9a47a421e105c20
Maximilian Grau <maximilian.grau@st.ovgu.de>
2021-10-15 12:21:28 +0200

    Add comments

--- include/util/led.hpp
+++ include/util/led.hpp
@@ -13,29 +13,39 @@ class LedBase
 public:
     virtual ~LedBase() = default;
 
+    /// Turn on LED, will call the inherited turn on process of derived class.
     void turnOn()
     {
         ledState = LedState::Normal;
         turnOnInherited();
     }
 
+    /// Turn off LED, will call the inherited turn off process of derived class.
     void turnOff()
     {
         ledState = LedState::Normal;
         turnOffInherited();
     }
 
+    /// Enable blinking at given frequency.
+    /// To turn off blinking simply use turnOn()/turnOff() functions.
+    /// \param frequency Blink frequency - 50% of period time will be on/off
     void setBlinking(units::si::Frequency frequency)
     {
         ledState = LedState::Blinking;
         this->blinkFrequency = frequency;
     }
 
+    /// Enable flashing.
+    /// To turn off flashing simply use turnOn()/turnOff() functions.
     void setFlashing()
     {
         ledState = LedState::Flashing;
     }
 
+    /// Updates the state of LED for blinking/flashing purposes.
+    /// Should be called periodically.
+    /// \param currentTicks the current system/task tick counter
     void updateState(uint32_t currentTicks)
     {
         switch (ledState)
@@ -64,7 +74,10 @@ public:
     }
 
 protected:
+    /// Each derived class should implement the turn on process if needed
     virtual void turnOnInherited() = 0;
+
+    /// Each derived class should implement the turn off process if needed
     virtual void turnOffInherited() = 0;
 
     enum class LedState
