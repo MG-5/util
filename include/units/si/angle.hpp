edd917276b1739a0a20be8f77817e88b03429f04
Michael Albrecht <micha.albrecht95@gmail.com>
2022-02-05 20:24:06 +0100

    Added degrees scale to angle

--- include/units/si/angle.hpp
+++ include/units/si/angle.hpp
@@ -1,13 +1,29 @@
 #pragma once
 
 #include "units/si.hpp"
+#include <cmath>
 
 namespace units::si
 {
 using Angle = Value<SiUnit<0, 0, 0, 0, 0, 0, 0>>;
 } // namespace units::si
 
+namespace
+{
+constexpr auto DegreesFactor = M_PIf32 / 180.0f;
+} // namespace
+
+namespace units::si::scale
+{
+constexpr auto deg = Scale<Angle::Unit>{DegreesFactor};
+} // namespace units::si::scale
+
 constexpr units::si::Angle operator"" _rad(const long double magnitude)
 {
     return units::si::Angle(magnitude);
+}
+
+constexpr units::si::Angle operator"" _deg(const long double magnitude)
+{
+    return units::si::Angle(magnitude * DegreesFactor);
 }
