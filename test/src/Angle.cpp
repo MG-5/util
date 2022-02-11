1f8f3ffe6f6d41924697418dcaab0fd8860e9031
Michael Albrecht <micha.albrecht95@gmail.com>
2022-02-11 22:58:56 +0100

    Util now requires c++20 for usage of std::numbers

--- test/src/Angle.cpp
+++ test/src/Angle.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 #include <units/si/angle.hpp>
+#include <numbers>
 
 using namespace units::si;
 
 TEST(units_angle, scalar)
 {
-    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(), Angle (M_PIf32/2).getMagnitude() );
-    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(), M_PIf32/2.0 );
-    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(scale::rad), Angle(M_PIf32/2.0).getMagnitude(scale::rad));
+    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(), Angle (std::numbers::pi/2).getMagnitude() );
+    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(), std::numbers::pi/2.0 );
+    EXPECT_FLOAT_EQ((90.0_deg).getMagnitude(scale::rad), Angle(std::numbers::pi/2.0).getMagnitude(scale::rad));
 }
