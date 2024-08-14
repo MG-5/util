5b9809ba1eec21d2ac14c2764bf26dc202f8ee4e
MG-5 <MG-5@users.noreply.github.com>
2024-08-14 14:09:21 +0200

    Use better variable name

--- include/util/led/GammaCorrection.hpp
+++ include/util/led/GammaCorrection.hpp
@@ -13,21 +13,22 @@ class GammaCorrection
 private:
     static constexpr auto StartOffset = 1.0f;
     static constexpr auto GammaFactor = 2.2f;
-    static constexpr auto NumberOfEntries = (1 << NumberOfResolutionBits) - 1;
 
 public:
-    using GammaTable = std::array<size_t, NumberOfEntries + 1>;
+    static constexpr auto MaxResolutionValue = (1 << NumberOfResolutionBits) - 1;
+    using GammaTable = std::array<size_t, MaxResolutionValue + 1>; // +1 for zero
 
     static constexpr GammaTable createGammaTable()
     {
         GammaTable gammaTable{};
 
-        for (auto i = 1; i < NumberOfEntries + 1; i++)
+        for (auto i = 1; i < MaxResolutionValue + 1; i++)
         {
-            const auto Logarithm =
-                gcem::pow(static_cast<float>(i) / static_cast<float>(NumberOfEntries), GammaFactor);
+            const auto Logarithm = gcem::pow(
+                static_cast<float>(i) / static_cast<float>(MaxResolutionValue), GammaFactor);
 
-            gammaTable[i] = StartOffset + gcem::round(Logarithm * (NumberOfEntries - StartOffset));
+            gammaTable[i] =
+                StartOffset + gcem::round(Logarithm * (MaxResolutionValue - StartOffset));
         }
 
         return gammaTable;
